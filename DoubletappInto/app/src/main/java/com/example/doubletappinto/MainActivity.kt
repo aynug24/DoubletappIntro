5be5a45448d04d762fae9b0c13889c4fce6def53
Сергей Луговых <lugowyh.serezha@yandex.ru>
2022-02-18 01:50:30 +0500

    Added second activity

--- DoubletappInto/app/src/main/java/com/example/doubletappinto/MainActivity.kt
+++ DoubletappInto/app/src/main/java/com/example/doubletappinto/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.doubletappinto
 
+import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+    var value: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        value++
+    }
+
+    fun squareClick(view: View) {
+        val intent = Intent(
+            this,
+            SquareActivity::class.java
+        ).apply {
+            val bundle = Bundle().apply { putString("VALUE", value.toString()) }
+            putExtras(bundle)
+        }
+
+        startActivity(intent)
     }
 }
